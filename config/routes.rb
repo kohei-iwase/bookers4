a546956e49350cdc391ac8d3e33be4aca020331f
kohei-iwase <me.tekelily@gmail.com>
2020-02-09 10:12:23 +0000

    [fix] routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 #最初のページ
-  root 'books#top'
+  root 'books#index'
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
   resources :users, only: [:new, :show,	  :edit,  :update, :index, :create]
 
